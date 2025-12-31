3a08ccc88bdd4eb90f3b0969043f51de1682b5f8
mohammed metlej <mhamad.metlej@std.balamand.edu.lb>
2025-12-31 18:31:39 +0200

    Implement booking creation endpoint with capacity validation

--- handlers/booking.go
+++ handlers/booking.go
@@ -1,10 +1,53 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"roomshare/db"
+	"roomshare/models"
 )
 
 func CreateBooking(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("CreateBooking endpoint reached"))
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req models.BookingRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var capacity int
+	var occupants int
+
+	query := `
+	SELECT r.capacity, COUNT(ro.user_id)
+	FROM rooms r
+	LEFT JOIN room_occupants ro ON r.id = ro.room_id
+	WHERE r.id = $1
+	GROUP BY r.capacity
+	`
+
+	err = db.DB.QueryRow(query, req.RoomID).Scan(&capacity, &occupants)
+	if err != nil {
+		http.Error(w, "Room is full", http.StatusConflict)
+		return
+	}
+
+	_, err = db.DB.Exec(
+		`Insert INTO bookings(room_id, user_id, start_date , end_date)
+		Values($1,$2,$3,$4)`,
+		req.RoomID, req.UserID, req.StartDate, req.EndDate,
+	)
+	if err != nil {
+		http.Error(w, "Failed to Create booking ", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Booking Request Created"))
 }
--- models/booking.go
+++ models/booking.go
@@ -1,1 +1,8 @@
 package models
+
+type BookingRequest struct {
+	RoomID    int    `json:"room_id"`
+	UserID    int    `json:"user_id"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+}
